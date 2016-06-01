eb597d61e1ac0e5a3f2b161cc07668bc5275b51d
Dennis Jose da Silva <dennisjosesilva@gmail.com>
2016-05-31 21:43:14 -0300

    implement of grayscale morphological operation like top hat e gradient

--- grayscale_morphology.py
+++ grayscale_morphology.py
@@ -13,5 +13,17 @@ def closing(f, b=bm.create_structure_element_cross()):
 def opening(f, b=bm.create_structure_element_cross()):
     return mm.grey_opening(f, structure=b)
 
+def morphological_external_boundary(f, b=bm.create_structure_element_cross()):
+    return f - erosion(f, b)
 
+def morphological_internal_boundary(f, b=bm.create_structure_element_cross()):
+    return dilation(f, b) - f
 
+def morphological_gradient(f, b=bm.create_structure_element_cross()):
+    return dilation(f, b) - erosion(f, b)
+
+def opening_top_hat(f, b=bm.create_structure_element_cross()):
+    return f - opening(f, b)
+
+def closing_top_hat(f, b=bm.create_structure_element_cross()):
+    return closing(f, b) - f
